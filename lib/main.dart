2b965e66ed792b0883a478e4bbf20e2df1cf2a28
ReiyaMori <hanedab787@gmail.com>
2021-02-17 19:17:05 +0900

    StatefulWidgetを使った記述

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,44 @@ import 'package:flutter/material.dart';
 void main()=>runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
+  final title = 'くまおさんアプリ';
+  final message = 'こんにちは、ユーザーさん';
+
   @override
   Widget build(BuildContext context){
     return new MaterialApp(
       title: "Kumasan",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('くまおさんアプリ'),
-        ),
-        body: Text(
-          'こんにちは、ユーザーさん',
-          style: TextStyle(fontSize: 32.0),
-        ),
-      )
+      home: new MyHomePage(
+        title: this.title,
+        message: this.message
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  final String title;
+  final String message;
+
+  MyHomePage({this.title, this.message}): super();
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title)
+      ),
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize: 32.0),
+      ),
     );
   }
 }

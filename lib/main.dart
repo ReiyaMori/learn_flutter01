8a75ccdf6c9fc32af704086ea22a5757b8112464
ReiyaMori <hanedab787@gmail.com>
2021-02-18 17:36:40 +0900

    Floating Action Button を使った、表示メッセージの変更

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,67 @@ import 'package:flutter/material.dart';
 void main()=>runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
+
   final title = 'くまおさんアプリ';
-  final message = 'こんにちは、ユーザーさん';
 
   @override
   Widget build(BuildContext context){
-    return new MaterialApp(
-      title: "Kumasan",
+    return MaterialApp(
+      title: this.title,
       home: new MyHomePage(
         title: this.title,
-        message: this.message
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget{
-  final String title;
-  final String message;
 
-  MyHomePage({this.title, this.message}): super();
+
+  MyHomePage({this.title}): super();
+
+  final String title;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage>{
+  String _message;
 
+  //メッセージ初期値の指定
+  @override
+  void initState(){
+    super.initState();
+    _message = 'こんにちは、ユーザーさん';
+  }
+
+  //変更後のメッセージ
+  void _setMessage(){
+    setState(() {
+      _message = 'くまおです';
+    });
+  }
+
+  //画面のビルド
   @override
   Widget build(BuildContext context){
 
     return Scaffold(
+
       appBar: AppBar(
-        title: Text(widget.title)
+        title: Text(widget.title),
       ),
+
       body: Text(
-        widget.message,
-        style: TextStyle(fontSize: 32.0),
+        _message,
+        style: TextStyle(fontSize: 25.0),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setMessage,
+        tooltip: 'set message.',
+        child: Icon(Icons.arrow_forward_ios),
       ),
     );
   }
